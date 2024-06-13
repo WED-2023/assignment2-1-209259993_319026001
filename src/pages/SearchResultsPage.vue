08f446c923099840b989335da12874baceb2bbee
lioraft <lioraftabi@gmail.com>
2024-06-14 00:25:56 +0300

    fixed search results page, added sorting by likes or time, and an error message when no results come back

--- src/pages/SearchResultsPage.vue
+++ src/pages/SearchResultsPage.vue
@@ -1,7 +1,27 @@
 <template>
-    <div>
-        <RecipeResultList :title="'Results'" :recipes="recipes" />
+  <div class="results-page">
+        <!-- if results are not empty, show them and give user option to sort -->
+        <div v-if="recipes.length > 0">
+          <div class="results-header">
+            <h2>Results</h2>
+          </div>
+          <!-- sorting based on time or likes -->
+          <div class="sort-container">
+              <label for="sort">Sort by:</label>
+              <select id="sort" v-model="sortCriteria" @change="sortRecipes">
+                  <option value="time">Make Time</option>
+                  <option value="likes">Popularity</option>
+              </select>
+          </div>
+          <!-- show results -->
+      <RecipeResultList :recipes="recipes" />
     </div>
+    <!-- if no results come back, show message -->
+    <div v-else>
+      <h1>No results came back.</h1>
+      <router-link to="/search" exact>Search again?</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -33,11 +53,66 @@ methods: {
       } catch (error) {
         console.error("Error fetching recipes:", error);
       }
-    }
+    },
+    // function to sort the results based on likes and time
+    sortRecipes() {
+            this.recipes.sort((a, b) => {
+                switch (this.sortCriteria) {
+                    case 'time':
+                       // from less time to more time, assuming less time to make is better
+                        return a.readyInMinutes - b.readyInMinutes;
+                    case 'likes':
+                      // from most likes to least likes, assuming higher number of likes is better
+                        return b.aggregateLikes - a.aggregateLikes;
+                    default:
+                      // if no sort, return regular results
+                        return 0;
+                }
+            });
+        }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.results-header, .sort-container {
+  display: flex;
+  justify-content: center;
+  align-items: center; 
+  width: 100%; 
+  padding: 5px; 
+}
 
+.results-page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+label {
+  margin-right: 10px;
+  font-weight: bold;
+  color: #2c3e50;
+  font-size: 18px;
+}
+
+select {
+  padding: 5px 10px;
+  border: 1px solid #2c3e50;
+  border-radius: 4px;
+  background-color: #f9f9f9;
+  font-size: 1em;
+  color: #333;
+  outline: none;
+  transition: border-color 0.3s, box-shadow 0.3s, background-color 0.3s;
+
+  &:hover {
+    background-color: #f1f1f1;
+    cursor: pointer;
+  }
+  &:focus {
+    border-color: #999;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+  }
+}
 </style>
